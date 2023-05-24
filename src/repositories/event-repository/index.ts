96ed039588f8ad008769f88892e70905ccab43ce
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-05-24 17:31:50 -0300

    refactory: Redis

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -2,15 +2,14 @@ import { prisma, redis } from '@/config';
 
 async function findFirst() {
   const cacheKey = 'event';
-  const cachedEvent = await redis.get(cacheKey);
-
-  if (cachedEvent) {
-    const event = JSON.parse(cachedEvent);
+  const cachedEvent = JSON.parse(await redis.get(cacheKey));
+  if (cachedEvent.id) {
+    const event = cachedEvent;
     return event;
   }
 
-  const event = prisma.event.findFirst();
-  redis.setEx(cacheKey, 3600, JSON.stringify(event));
+  const event = await prisma.event.findFirst();
+  redis.set(cacheKey, JSON.stringify(event));
 
   return event;
 }
